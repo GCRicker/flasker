b119e885414eec47dd6f0f0b89e1e0c6b235ed3a
Gregs Laptop <GCRicker@comcast.net>
2021-03-19 21:09:26 -0400

    Up to and including lesson 8

--- hello.py
+++ hello.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 #  Need to start Flask by...
 # 1. start powershell
 # 1a. type bash
-# 2. type export FLASK_APP=hello.py
-# 3. type export FLASK_ENV=development
-# 4. type flask run
+# 2. type "export FLASK_APP=hello.py"
+# 3. type "export FLASK_ENV=development"
+# 4. type "flask run"
+# CSS changes use cntrl F5 for new cache
 
 # Configure GitHub
 # $ git config --global push.default matching
@@ -17,19 +20,42 @@ from wtforms.validators import DataRequired
 # $ git add .
 # $ git commit -am 'comment'
 
-
+#
 # Create a Flask Instance
 app = Flask(__name__)
+# Add Database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+
+# Secret Key
 app.config["SECRET_KEY"] = "My secret key for csrf used to protect forms"
+
+# Initialize the Database
+db = SQLAlchemy(app)
+
+# Create a database Model
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # Create a String
+    def __repr__(self):
+        return "<Name %r>" % self.name
+
 
 # Create a Form Class
 class NamerForm(FlaskForm):
-    name = StringField("What's your name?", validators=[DataRequired()])
+    name = StringField("What is your name?", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
 
-# Create a route decorator
-@app.route("/")
+# Create a Form Class
+class UserForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 # def index():
 #     return "<h1>Hello World!</h1>"
@@ -44,6 +70,27 @@ class NamerForm(FlaskForm):
 # striptags - Remove any tags that are trying to be passed
 
 
+@app.route("/user/add", methods=["GET", "POST"])
+def add_user():
+    name = None
+    form = UserForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ""
+        form.email.data = ""
+        flash("User Added Successfully!")
+
+    our_users = Users.query.order_by(Users.date_added)
+    return render_template("add_user.html", form=form, name=name, our_users=our_users)
+
+
+# Create a route decorator
+@app.route("/")
 def index():
     first_name = "Gregory"
     stuff = "This is <strong>BOLD</strong> Text"
@@ -87,4 +134,5 @@ def name():
     if form.validate_on_submit():
         name = form.name.data
         form.name.data = ""  # Clear it out for the next form use
+        flash("Form Submitted Successfully")
     return render_template("name.html", name=name, form=form)
---
+++ static/js/myfile.js
@@ -0,0 +1,1 @@
+document.getElementById('demo').innerHTML = "This is created by Javascript"
