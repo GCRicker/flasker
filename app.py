1c6023f61a2f36b17ea0e66fd673dddccf48cdd8
GCRicker <GCRicker@comcast.net>
2022-01-28 20:11:45 -0500

    Fixed Profile Picture

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ def dashboard():
 
         try:
             db.session.commit()
-            saver.save(os.path.join(app.config["UPLOAD_FOLDER"]), pic_name)
+            saver.save(os.path.join(app.config["UPLOAD_FOLDER"], pic_name))
             flash("User Updated Successfully")
             return render_template("dashboard.html", form=form, name_to_update=name_to_update)
         except:
