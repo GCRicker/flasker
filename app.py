111134863cfbd8db937b35c640557ed254eae251
GCRicker <GCRicker@comcast.net>
2022-02-04 12:23:24 -0500

    Profile Picture Work

--- app.py
+++ app.py
@@ -149,24 +149,31 @@ def dashboard():
         name_to_update.favorite_color = request.form["favorite_color"]
         name_to_update.username = request.form["username"]
         name_to_update.about_author = request.form["about_author"]
-        name_to_update.profile_pic = request.files["profile_pic"]
-        # Grab Image Name
-        pic_filename = secure_filename(name_to_update.profile_pic.filename)
-        # Set UUID
-        pic_name = str(uuid.uuid1()) + "_" + pic_filename
-        # Save that Image
-        saver = request.files["profile_pic"]
 
-        # Save to string to save to db
-        name_to_update.profile_pic = pic_name
+        # Check for profile image
+        if request.files["profile_pic"]:
+            name_to_update.profile_pic = request.files["profile_pic"]
+            # Grab Image Name
+            pic_filename = secure_filename(name_to_update.profile_pic.filename)
+            # Set UUID
+            pic_name = str(uuid.uuid1()) + "_" + pic_filename
+            # Save that Image
+            saver = request.files["profile_pic"]
 
-        try:
+            # Save to string to  save to db
+            name_to_update.profile_pic = pic_name
+
+            try:
+                db.session.commit()
+                saver.save(os.path.join(app.config["UPLOAD_FOLDER"], pic_name))
+                flash("User Updated Successfully with profile picture change")
+                return render_template("dashboard.html", form=form, name_to_update=name_to_update)
+            except:
+                flash("Error!  Looks like there was a problem")
+                return render_template("dashboard.html", form=form, name_to_update=name_to_update)
+        else:
             db.session.commit()
-            saver.save(os.path.join(app.config["UPLOAD_FOLDER"], pic_name))
-            flash("User Updated Successfully")
-            return render_template("dashboard.html", form=form, name_to_update=name_to_update)
-        except:
-            flash("Error!  Looks like there was a problem")
+            flash("User Updated Successfully with no profile pic changes")
             return render_template("dashboard.html", form=form, name_to_update=name_to_update)
     else:
         return render_template("dashboard.html", form=form, name_to_update=name_to_update, id=id)
